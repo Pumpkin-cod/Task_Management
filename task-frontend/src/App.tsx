40d69c97bc5da40740505ac2a274c014e47b71ad
Catherine <katieboye44@gmail.com>
2025-06-04 11:31:17 +0000

    updated

---
+++ task-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+
+const App = () => {
+  return (
+    <div>
+      hello world
+      <p>Welcome to the Task Management App</p>
+      <p>Manage your tasks efficiently!</p>
+    </div>
+  )
+}
+
+export default App
+
+
---
+++ task-frontend/src/main.tsx
@@ -0,0 +1,11 @@
+
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { Amplify } from 'aws-amplify'; 
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig); 
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(<App />);
+
